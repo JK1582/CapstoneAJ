3314307171cee4ed32a393c8af97627f58f7128f
autumnklein8 <autumnklein8@gmail.com>
2022-02-10 21:13:46 -0500

    add in error handling

--- src/App.java
+++ src/App.java
@@ -1,7 +1,9 @@
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -17,7 +19,7 @@ public class App {
                 Statement stmt = conn.createStatement();) {
 
             // --CREATE TABLE TO HOLD COMPANY EMPLOYEE INFO--
-
+                    //this is going to act as like "logging in" for the company... lets remember that for the GUI 
             Scanner myObj = new Scanner(System.in);
             String company_name;
             System.out.println("Enter company name: "); // user input of employee name
@@ -26,39 +28,58 @@ public class App {
 
             // TODO: add error checking logic to see if table already exists etc.
 
-            String sql_table = "CREATE TABLE " + company_name + "_employees " +
-                    "(id INTEGER not NULL, " +
-                    " fname VARCHAR(255), " +
-                    " lname VARCHAR(255), " +
-                    " email VARCHAR(255), " +
-                    " PRIMARY KEY ( id ))";
+           //TODO: add error checking logic to see if table already exists etc. 
+			String tblName = company_name + "_employees";
+			DatabaseMetaData dbm = conn.getMetaData();
+			ResultSet tables = dbm.getTables(null, null, tblName, null);
+			if(tables.next()) {
+				//if table exists
+				System.out.println("Company already registered");
+				
+				//TODO: give them options to add employee, update employee info, remove employee
+			}
+			else {
+				//table does not exist
+				System.out.println("Register company");
+				String sql_table = "CREATE TABLE " + company_name + "_employees " +
+		                   "(id INTEGER not NULL, " +
+		                   " fname VARCHAR(255), " + 
+		                   " lname VARCHAR(255), " + 
+		                   " email VARCHAR(255), " + 
+		                   " PRIMARY KEY ( id ))"; 
+				
+				stmt.executeUpdate(sql_table);
+				System.out.println("Created table in database");
+				
+				//TODO: Joe add in code to give option to add more employees
+				//if you can, add code so can upload excel or csv file!
+				
+				//--ADD IN EMPLOYEE DATA TO TABLE--
+				System.out.println("Enter id: ");
+				String empId = myObj.nextLine();
+				System.out.println("Enter first name: ");
+				String firstName = myObj.nextLine();
+				System.out.println("Enter last name: ");
+				String lastName = myObj.nextLine();
+				System.out.println("Enter email: ");
+				String empEmail = myObj.nextLine();
+				
 
-            stmt.executeUpdate(sql_table);
-            System.out.println("Created table in database");
+			      // the mysql insert statement
+			      String query = " insert into " + company_name + "_employees  (id, fname, lname, email)"
+			        + " values (?, ?, ?, ?)";
 
-            // --ADD IN EMPLOYEE DATA TO TABLE--
-            System.out.println("Enter id: ");
-            String empId = myObj.nextLine();
-            System.out.println("Enter first name: ");
-            String firstName = myObj.nextLine();
-            System.out.println("Enter last name: ");
-            String lastName = myObj.nextLine();
-            System.out.println("Enter email: ");
-            String empEmail = myObj.nextLine();
-
-            // the mysql insert statement
-		      String query = " insert into " + company_name + "_employees  (id, fname, lname, email)"
-              + " values (?, ?, ?, ?)";
-
-            // create the mysql insert preparedstatement
-            PreparedStatement preparedStmt = conn.prepareStatement(query);
-            preparedStmt.setString (1, empId);
-            preparedStmt.setString (2, firstName);
-            preparedStmt.setString   (3, lastName);
-            preparedStmt.setString    (4, empEmail);
-            preparedStmt.execute();
-            // TODO: should we give user ability to upload csv or stick to one by one entries?
-            System.out.println("Data inserted.");
+			      // create the mysql insert preparedstatement
+			      PreparedStatement preparedStmt = conn.prepareStatement(query);
+			      preparedStmt.setString (1, empId);
+			      preparedStmt.setString (2, firstName);
+			      preparedStmt.setString   (3, lastName);
+			      preparedStmt.setString    (4, empEmail);
+			      preparedStmt.execute();
+				
+				System.out.println("Data inserted.");
+			}
+            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
