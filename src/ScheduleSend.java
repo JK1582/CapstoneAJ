195e37a83ca3429b1cb96bbc341fb9e2a70b04a5
autumnklein8 <autumnklein8@gmail.com>
2022-04-27 13:37:26 -0400

    fixed

--- src/ScheduleSend.java
+++ src/ScheduleSend.java
@@ -1,3 +1,4 @@
+package gui;
 import java.util.Date;
 
 import org.quartz.Job;
@@ -6,26 +7,25 @@ import org.quartz.JobExecutionException;
 
 public class ScheduleSend implements Job {
 	private static String USER_NAME = "JkelleyAKlein"; // GMail user name (just the part before "@gmail.com")
-	private static String PASSWORD = "JKelleyAKlein1!"; // GMail password
-	App a = new App();
+    private static String PASSWORD = "JKelleyAKlein1!"; // GMail password
+	App gd = new App();
 	SendEmail sd = new SendEmail();
-	TrackingPixel = new TrackingPixel();
-
+	TrackingPixel tp = new TrackingPixel();
 	@Override
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
-		String email = a.ceoEmail;
+		
+		String email = gd.ceoEmail;
 		String from = USER_NAME;
-		String pass = PASSWORD;
-		String subject = "Phishing Report";
-		String body = "<html> Hello, <br>"
-		+ "Here is your requested phishing report. <br> <br>"
-		+ TrackingPixel.reportBody 
-		+ " <br> We suggest forwarding <a href= 'youtube.com/watch?v=WNVTGTrWcvw'> this video </a> to the above employees to prevent them from falling for a legitimate phishing attack in the future. "
-		+ "<br> <br> Stay safe. Stay secure. </html>"; 
-		sd.sendFromGMail(email, subject, body);
+        String pass = PASSWORD;
+        String subject = "Phishing Report";
+        String body = "<html> Hello, <br>"
+        		+ "Here is your requested phishing report. <br> <br>"
+        		+ TrackingPixel.reportBody 
+        		+ " <br> We suggest forwarding <a href= 'youtube.com/watch?v=WNVTGTrWcvw'> this video </a> to the above employees to prevent them from falling for a legitimate phishing attack in the future. "
+        		+ "<br> <br> Stay safe. Stay secure. </html>";
+        sd.sendFromGMail(email, subject, body);
 		System.out.println("Scheduler running");
 		System.out.println("The time is " + new Date());
-
 	}
 
 }
